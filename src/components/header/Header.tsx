8e97c69e32477bbfcabc9c68eac49c0381953eb6
CANE4KA <145180300+CANE4KA@users.noreply.github.com>
2024-10-12 23:04:33 +0500

    Modified index.html

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
-				<a href='/'>ToDo</a>
+				<a href='/'>Todo App by CANE4KA</a>
 			</div>
 		</header>
 	)
